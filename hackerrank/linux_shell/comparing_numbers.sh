86407077f3f488f59e9ba1d8d8376201dec6c2de
MTO <marc@mto.nu>
2020-03-16 21:56:59 +0100

    HackerRank linux shell exercise 6

--- hackerrank/linux_shell/comparing_numbers.sh
+++ hackerrank/linux_shell/comparing_numbers.sh
@@ -3,3 +3,24 @@
 # HackerRank - Linux Shell - Excercise 6
 # Author: MTO (https://www.mto.nu)
 
+declare -i X
+declare -i Y
+
+#echo -n "Enter X: "
+read X
+#echo -n "Enter Y: "
+read Y
+
+
+if [[ $X -lt $Y ]]; then
+    echo "X is less than Y"
+elif [[ $X -gt $Y ]]; then
+    echo "X is greater than Y"
+elif [[ $X -eq $Y ]]; then
+    echo "X is equal to Y"
+fi
+
+#Much shorter Solution (&& if first statement is true then second statement)
+[[ $X -gt $Y ]] && echo 'X is greater than Y'
+[[ $X -eq $Y ]] && echo 'X is equal to Y'
+[[ $X -lt $Y ]] && echo 'X is less than Y'
